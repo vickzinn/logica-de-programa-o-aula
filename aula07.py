39c0dc9b546d4923b7f62308c000458e5253284f
vitor lopes <vitor.i.lopes@aluno.senai.br>
2025-09-02 12:07:46 -0300

    Add files via upload

--- aula07.py
+++ aula07.py
@@ -7,60 +7,63 @@ corrente que começa com saldo de R$ 0,00. O usuario tera as opções de: criar 
 exibir dados da conta FEITO, depositar FEITO, sacar FEITO e sair FEITO .
 '''
 
-cadastro = input('Digite seu nome: ')
-senha = input('Digite sua senha: ')
+cadastro = input('Digite seu nome: ').title().strip()
+senha = input('Digite sua senha: ').strip()
 
-print('Cadastro realizado')
-def criar_conta():
-    global saldo
-    saldo = 0.0
-    print('Conta criada...')
+try:
+    print('Cadastro realizado')
+    def criar_conta():
+        global saldo
+        saldo = 0.0
+        print('Conta criada...')
 
-def mostrar_dados():
-    print(f'Nome: {cadastro}')
-    print(f'Saldo: R$ {saldo:.2f}')
+    def mostrar_dados():
+        print(f'Nome: {cadastro}')
+        print(f'Saldo: R$ {saldo:.2f}')
 
-def depositar():
-    global saldo
-    valor = float(input('Digite um valor para depositar: R$ '))
-    if valor > 0:
-        saldo += valor
-        print(f'Depósito realizado! Novo saldo: R$ {saldo:.2f}')
-    else:
-        print('Valor inválido!')
-def sacar():
-    global saldo
-    valor = float(input('Digite o valor para sacar: R$ '))
-    if 0 < valor <= saldo:
-        saldo -= valor
-        print(f'Saque realizado! Novo saldo: R$ {saldo:.2f}')
-    else:
-        print('Saldo insuficiente ou valor inválido!')
-def sair():
-    print('Saindo...')
-    exit()
+    def depositar():
+        global saldo
+        valor = float(input('Digite um valor para depositar: R$ '))
+        if valor > 0:
+            saldo += valor
+            print(f'Depósito realizado! Novo saldo: R$ {saldo:.2f}')
+        else:
+            print('Valor inválido!')
+    def sacar():
+        global saldo
+        valor = float(input('Digite o valor para sacar: R$ '))
+        if 0 < valor <= saldo:
+            saldo -= valor
+            print(f'Saque realizado! Novo saldo: R$ {saldo:.2f}')
+        else:
+            print('Saldo insuficiente ou valor inválido!')
+    def sair():
+        print('Saindo...')
+        exit()
 
-while True:
-    print('--- MENU DO SEU BANCO ---')
-    print('1 - Criar conta bancaria')
-    print('2 - Mostrar dados')
-    print('3 - Depositar')
-    print('4 - Sacar')
-    print('5 - Sair')
-    opcao = input('Escolha uma opção: ')
-    if opcao == '1':
-        criar_conta()
-    elif opcao == '2':
-        mostrar_dados()
-    elif opcao == '3':
-        depositar()
-    elif opcao == '4':
-        sacar()
-    elif opcao == '5':
-        sair()
-    else:
-        print('Opção inválida!')
+    while True:
+        print('--- MENU DO SEU BANCO ---')
+        print('1 - Criar conta bancaria')
+        print('2 - Mostrar dados')
+        print('3 - Depositar')
+        print('4 - Sacar')
+        print('5 - Sair')
+        opcao = input('Escolha uma opção: ')
+        if opcao == '1':
+            criar_conta()
+        elif opcao == '2':
+            mostrar_dados()
+        elif opcao == '3':
+            depositar()
+        elif opcao == '4':
+            sacar()
+        elif opcao == '5':
+            sair()
+        else:
+            print('Opção inválida!')
 
+except Exception as e:
+    print(f'Ocorreu um erro: {e}')
 '''
 o def funciona para definir funções que podem ser chamadas em diferentes partes do código.
 exemplo:
